df89810ae19d63a7d551492b1d0b53e92aa349bb
Jack Pflaum <jackpflaum@gmail.com>
2023-06-22 19:23:45 +0800

    home, job_positions and job_details test methods

--- RecruitmentWebsite/jobs/tests.py
+++ RecruitmentWebsite/jobs/tests.py
@@ -1,9 +1,70 @@
 from django.test import TestCase
-from django.urls import reverse
+from django.urls import reverse, resolve
+from .views import home, job_details, job_positions
+from .models import JobPositions
 
 
-class HomePageTest(TestCase):
-    def test_status_code(self):
+class HomeTests(TestCase):
+    """Tests for home view"""
+
+    def test_home_view_status_code(self):
         url = reverse('home')
-        self.response = self.client.get(url)
-        self.assertEqual(self.response.status_code, 200)
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_url_resolves_home_view(self):
+        """the purpose is to verify that the root URL correctly mapped to the 'home' view"""
+        view = resolve('/')
+        self.assertEqual(view.func, home)
+
+
+class JobPositionsTests(TestCase):
+    """Tests for job_positions view"""
+
+    def setUp(self):
+        """create JobPositions instance for test cases"""
+        JobPositions.objects.create(job_title='Carpenter', company='Wood Inc', location='Perth', closing_date='1994-06-14')
+
+    def test_job_positions_view_success_status(self):
+        url = reverse('job_positions')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+    
+    def test_job_positions_url_resolves_job_positions_view(self):
+        """checking if the url pattern is resolving the 'job_positions' view"""
+        view = resolve('/job_positions/')
+        self.assertEqual(view.func, job_positions)
+
+
+    # currently failing.
+    # def test_job_positions_search_box(self):
+    #     keyword_search = 'Carpenter'
+    #     url = reverse('job_positions')
+    #     response = self.client.get(url, {'search': keyword_search})
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertContains(response, keyword_search)
+
+
+class JobDetailsTest(TestCase):
+    """Tests for job_details view"""
+
+    def setUp(self):
+        """create JobPositions instance for test cases"""
+        JobPositions.objects.create(job_title='Carpenter', company='Wood Inc', location='Perth', closing_date='1994-06-14')
+    
+    def test_job_details_view_success_status(self):
+        """returning status code 200 for an existing JobPositions object"""
+        url = reverse('job_details', kwargs={'id': 1})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_job_details_view_not_found_status_code(self):
+        """returning status code 404 for a JobPositions object that doesn't exist"""
+        url = reverse('job_details', kwargs={'id': 999})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+    
+    def test_job_details_url_resolves_job_details_view(self):
+        """checking if the url pattern is correctly resolving the 'job_details' view"""
+        view = resolve('/job_details/1')
+        self.assertEqual(view.func, job_details)
